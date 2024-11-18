14a642feac6a432db19bd797383766297dd5c2cf
F-Duda <frantisekdudain@gmail.com>
2024-11-18 10:07:13 +0100

    # příkaz cat # cat nazev_souboru - vypise obsah souboru # cat nazev_souboru1.txt > nazev_souboru2.txt -> kopirovani obsahu souboru do druheho, pokud nazev_souboru2.txt neexistuje vytvori jej # cat nazev_souboru1.txt >> nazev_souboru2.txt -> presune obsah # cat > nazev_souboru -> textová úprava souboru alias uprav=”cat >”
    # příkaz date -> zobrazí datum
    alias datum="date"
    
    # příkaz find -> najde soubor
    alias najdi="find"
    
    # příkaz mv -> přejmenování
    alias zmen_jmeno="mv"
    
    # příkaz rm -> smazáni souboru
    alias smaz="rm"
    
    # příkaz uname ->zobrazí název OS
    alias os="uname"
    
    # příkaz uptime -> doba spuštění OS
    alias doba_spusteni="uptime"

--- src/system_info.sh
+++ src/system_info.sh
@@ -1,6 +1,24 @@
-alias os="uname"
+# příkaz cat 
+# cat nazev_souboru - vypise obsah souboru 
+# cat nazev_souboru1.txt > nazev_souboru2.txt -> kopirovani obsahu souboru do druheho, pokud nazev_souboru2.txt neexistuje vytvori jej 
+# cat nazev_souboru1.txt >> nazev_souboru2.txt -> presune obsah
+# cat > nazev_souboru -> textová úprava souboru
+alias uprav=”cat >”
+
+# příkaz date -> zobrazí datum
 alias datum="date"
-alias doba_spusteni="uptime"
-alias smaz="rm"
-alias zmen_jmeno="mv"
+
+# příkaz find -> najde soubor
 alias najdi="find"
+
+# příkaz mv -> přejmenování
+alias zmen_jmeno="mv"
+
+# příkaz rm -> smazáni souboru
+alias smaz="rm"
+
+# příkaz uname ->zobrazí název OS
+alias os="uname"
+
+# příkaz uptime -> doba spuštění OS
+alias doba_spusteni="uptime"
