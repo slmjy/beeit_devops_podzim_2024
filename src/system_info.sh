0aa1235f5b9122f788c1f41e4454bdfb0eddde65
Martin Kucin <52672224+Mart-Kuc@users.noreply.github.com>
2024-11-18 18:27:40 +0100

    Update system_info.sh
    Commit message

--- src/system_info.sh
+++ src/system_info.sh
@@ -1,10 +1,1 @@
-# Sem vložte kód příkazů, které zjistí více informací o Linux systému a hezky je vypíší
-uname -s
-uname -r
-uname -v
-uname -n
-uname -m
-uname -p
-uname -i
-uname -o
-uname -a
+echo "test Ahoj"
